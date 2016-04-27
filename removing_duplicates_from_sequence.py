9b10128c185ec5fbe1b33fc7ee2d8cb0c8aaf4f7
codyowl <codyowl@gmail.com>
2016-04-27 10:46:15 +0530

    Added removing duplicates in a sequence

---
+++ removing_duplicates_from_sequence.py
@@ -0,0 +1,11 @@
+random_list = [1,2,3,3,4,5,5,6,7,7,8,8]
+
+#Using set we can remove duplicates from a sequence
+
+remove_duplicates = set(random_list)
+
+convert_to_list = list(remove_duplicates)
+
+print convert_to_list
+
+
